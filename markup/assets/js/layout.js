bf8e02afa13f6537cbc59d34ab3841c6c383aa6d
강효선 <kanghyoseon@ganghyoseon-ui-MacBook-Pro.local>
2021-05-11 23:48:42 +0900

    add|modify: added markup, modify npm script

---
+++ markup/assets/js/layout.js
@@ -0,0 +1,11 @@
+$(function(){
+    // layout
+    $('#nav').load('../../../docs/layout/nav.html');
+    $('#footer').load('../../../docs/layout/footer.html');
+
+    // components
+    $('.feed-item-area').load('../../../docs/components/feed-item.html');
+    $('.pagination-area').load('../../../docs/components/pagination.html');
+    $('.my-profile-area').load('../../../docs/components/my-profile.html');
+    $('.view-modal-area').load('../../../docs/components/view-modal.html');
+})
